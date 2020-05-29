03ad7e33602aeede9eca7f9d569f69c946a9a19a
Scott Linden <sdlinden@gmail.com>
2020-05-29 07:45:06 -0500

    Moved setupDesktop.sh to scripts

--- setup.sh
+++ setup.sh
@@ -171,6 +171,40 @@ function setupGnomeExt {
   gsettings set org.gnome.shell.extensions.user-theme name 'Yaru-Grey-dark'
 }
 
+function setupDesktop {
+  ## Setting the desktop background
+  sudo cp ${PROJDIR}/WaterDropsOnGrey.jpeg ${BGDIR}
+
+  gsettings set org.gnome.desktop.background picture-uri file://${BGDIR}/WaterDropsOnGrey.jpeg
+  gsettings set org.gnome.desktop.background picture-options 'zoom'
+  gsettings set org.gnome.desktop.screensaver picture-uri file://${BGDIR}/WaterDropsOnGrey.jpeg
+  gsettings set org.gnome.desktop.screensaver picture-options 'zoom'
+
+  gsettings set org.gnome.desktop.background show-desktop-icons 'true'
+
+  gsettings set org.gnome.desktop.datetime automatic-timezone 'true'
+
+  gsettings set org.gnome.desktop.interface clock-format '12h'
+  gsettings set org.gnome.desktop.interface clock-show-date 'true'
+  gsettings set org.gnome.desktop.interface clock-show-seconds 'true'
+  gsettings set org.gnome.desktop.interface show-battery-percentage 'true'
+
+  gsettings set org.gnome.desktop.peripherals.mouse natural-scroll 'true'
+
+  gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+  gsettings set org.gnome.desktop.wm.preferences theme 'Adwaita'
+
+  #gsettings set org.gnome.GWeather default-location '','KLRJ',nothing
+  #gsettings set org.gnome.GWeather pressure-unit 'inch-hg'
+  #gsettings set org.gnome.GWeather speed-unit 'mph'
+  #gsettings set org.gnome.GWeather temperature-unit 'fahrenheit'
+
+  gsettings set org.gnome.desktop.interface gtk-theme "Cloudy-Light-Grey"
+  gsettings set org.gnome.desktop.interface icon-theme 'Yaru-Grey'
+  gsettings set org.gnome.desktop.interface cursor-theme 'Yaru-Grey'
+}
+
+
 ########
 # MAIN #
 ########
@@ -209,7 +243,7 @@ getThemes
 setupGnomeExt
 
 ## Setup desktop
-sh ./setupDesktop.sh
+setupDesktop
 
 ## Setup Terminal
 sh ./setupTerminal.sh
