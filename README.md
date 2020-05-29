b2227e418a80a1bff9363055e59cb993a04640a2
Scott Linden <sdlinden@gmail.com>
2020-05-29 13:11:03 -0500

    Added installation instructions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Title
+# Installation
+
+You can clone the git repo and run the setup.sh
+
+   git clone ${PROJECT}
+
+or
+
+You can download the setup.sh with wget and rune the setup.sh file.
+
+   wget --no-check-certificate https://raw.githubusercontent.com/sdlinden/config/master/setup.sh
+   ./setup.sh
+
+or
+
+You can run the setup.sh directly from githup with curl.
+
+   curl -kL https://raw.githubusercontent.com/sdlinden/config/master/setup.sh
+
+These commands bypass the certificate error.  You should inspect any
+shell script you download from the internet before running.
--- setup.sh
+++ setup.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+################################################################################
+# Installation
+#
+# You can clone the git repo and run the setup.sh
+#
+#   git clone ${PROJECT}
+#
+# or
+#
+# You can download the setup.sh with wget and rune the setup.sh file.
+#
+#   wget --no-check-certificate https://raw.githubusercontent.com/sdlinden/config/master/setup.sh
+#   ./setup.sh
+#
+# or
+#
+# You can run the setup.sh directly from githup with curl.
+#
+#   curl -kL https://raw.githubusercontent.com/sdlinden/config/master/setup.sh
+#
+# These commands bypass the certificate error.  You should inspect any
+# shell script you download from the internet before running.
+#
+################################################################################
+
 #############
 # VARIABLES #
 #############
